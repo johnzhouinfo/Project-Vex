77c489026b32cf44c85e3b6afe1da02be3767ea2
JohnZhou <work@johnzhou.info>
2020-02-12 17:41:58 -0500

    feat: * Implemented the structure of the editor page
    Signed-off-by: JohnZhou <work@johnzhou.info>

--- editor.php
+++ editor.php
@@ -1,4 +1,31 @@
-<?php
-$result = pg_connect("host=localhost dbname=c4f00g04 user=c4f00g04 password=A-v9s4C&E7")
-or die("Can't connect to database" . pg_last_error());
-echo $result;
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link rel="icon" href="favicon.ico">
+    <base href="">
+    <title>Vex</title>
+
+    <link href="css/editor.css" rel="stylesheet">
+
+</head>
+<body>
+
+
+<div id="vex-builder">
+    <div id="vex-nav">NAV</div>
+    <div id="vex-left-top-project-list">Project List</div>
+    <div id="vex-compoment">Compoments</div>
+    <div id="vex-page">
+        <iframe style="width:100%;height:100%" src="http://www.baidu.com"></iframe>
+    </div>
+    <div id="vex-toolbar">Toolbar</div>
+</div>
+
+</body>
+</html>
