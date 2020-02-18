d4bf46b1c9783f342e2fdc263a2fd61426f11a46
JohnZhou <work@johnzhou.info>
2020-02-18 14:35:24 -0500

    test code
    Signed-off-by: JohnZhou <work@johnzhou.info>

--- editor.php
+++ editor.php
@@ -19,13 +19,76 @@
 
 <div id="vex-builder">
     <div id="vex-nav">NAV</div>
-    <div id="vex-left-top-project-list">Project List</div>
-    <div id="vex-compoment">Compoments</div>
+    <div id="vex-left-top-project-list">Project List
+        <div class="drop">Place Here</div>
+    </div>
+    <div id="vex-component">
+        <table>
+            <tr>
+                <td>
+                    <div class="component">
+                        <img src="img/empty-avatar.png" alt="avatar"/>
+                    </div>
+                </td>
+            </tr>
+            <!--            <tr>-->
+            <!--                <td>-->
+            <!--                    <div class = "component">-->
+            <!--                        <p>Item2</p>-->
+            <!--                    </div>-->
+            <!--                </td>-->
+            <!--            </tr>-->
+            <!--            <tr>-->
+            <!--                <td>row 3</td>-->
+            <!--            </tr>-->
+            <!--            <tr>-->
+            <!--                <td>row 4</td>-->
+            <!--            </tr>-->
+            <!--            <tr>-->
+            <!--                <td>row 5</td>-->
+            <!--            </tr>-->
+            <!--            <tr>-->
+            <!--                <td>row 6</td>-->
+            <!--            </tr>-->
+            <!--            <tr>-->
+            <!--                <td>row 7</td>-->
+            <!--            </tr>-->
+            <!--            <tr>-->
+            <!--                <td>row 8</td>-->
+            <!--            </tr>-->
+        </table>
+    </div>
+
     <div id="vex-page">
-        <iframe style="width:100%;height:100%" src="http://www.baidu.com"></iframe>
+        <iframe class="drop" style="width:100%;height:100%" src="https://johnzhou.info"></iframe>
     </div>
     <div id="vex-toolbar">Toolbar</div>
 </div>
+
+<script src="https://code.jquery.com/jquery-1.12.4.js"></script>
+<script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
+<script>
+    $(function () {
+        $(".component").draggable({
+            appendTo: "body",
+            cursor: "move",
+            helper: 'clone',
+            revert: "true",
+
+        });
+        $(".drop, iframe html div span").droppable({
+            tolerance: "intersect",
+            accept: ".component",
+            activeClass: "ui-state-default",
+            hoverClass: "ui-state-hover",
+            drop: function (event, ui) {
+                //$(this).append($(ui.draggable).clone());
+                $(ui.draggable).clone().insertAfter($(this));
+            }
+        });
+    });
+
+</script>
 
 </body>
 </html>
