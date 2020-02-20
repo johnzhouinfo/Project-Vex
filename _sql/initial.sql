c42995dc240f8adc276ad3e3115ed1b424d34b37
JohnZhou <work@johnzhou.info>
2020-02-19 23:28:28 -0500

    Refactor SQL code
    Signed-off-by: JohnZhou <work@johnzhou.info>

--- _sql/initial.sql
+++ _sql/initial.sql
@@ -1,41 +1,51 @@
--- Table: public."Component"
-
--- DROP TABLE public."Component";
-
-CREATE TABLE public."Component"
+-- User table
+CREATE TABLE public.vex_user
 (
-    "CompoentID" numeric(10,0) NOT NULL,
-    "Is_enable" character(255) COLLATE pg_catalog."default",
-    "CName" character(255) COLLATE pg_catalog."default",
-    "CIcon" character(255) COLLATE pg_catalog."default",
-    "Code" character(255) COLLATE pg_catalog."default",
-    "CIs_Delete" numeric(2,0),
-    CONSTRAINT "Component_pkey" PRIMARY KEY ("CompoentID")
+    "user_id" numeric(10,0)[] NOT NULL,
+    "username" character(255) COLLATE pg_catalog."default" NOT NULL,
+    "password" character(255) COLLATE pg_catalog."default",
+    "name" character(255) COLLATE pg_catalog."default",
+    "email" character(255) COLLATE pg_catalog."default",
+    "type" character(255) COLLATE pg_catalog."default",
+    "icon" character(255) COLLATE pg_catalog."default",
+    "creat_time" date,
+    "is_enable" character(255) COLLATE pg_catalog."default",
+    CONSTRAINT vex_user_pkey PRIMARY KEY ("user_id")
 )
 WITH (
     OIDS = FALSE
 )
 TABLESPACE pg_default;
 
-ALTER TABLE public."Component"
-    OWNER to postgres;
+-- Component table
+CREATE TABLE public.vex_component
+(
+    "component_id" numeric(10,0) NOT NULL,
+    "component_name" character(255) COLLATE pg_catalog."default",
+    "icon" character(255) COLLATE pg_catalog."default",
+    "code" character(255) COLLATE pg_catalog."default",
+    "is_enable" character(255) COLLATE pg_catalog."default",
+    "is_delete" numeric(2,0),
+    CONSTRAINT "component_pkey" PRIMARY KEY ("component_id")
+)
+WITH (
+    OIDS = FALSE
+)
+TABLESPACE pg_default;
 
--- Table: public.vex_product
-
--- DROP TABLE public.vex_product;
-
+-- Product Table
 CREATE TABLE public.vex_product
 (
-    "ProductID" numeric(10,0) NOT NULL,
-    "UserID" numeric(10,0)[] NOT NULL,
-    "Product_Name" character(255) COLLATE pg_catalog."default",
-    "Time" date,
-    "HTML/CSS" character(255) COLLATE pg_catalog."default",
-    "Is_Live" numeric(2,0),
-    "Is_Delete" numeric(2,0),
-    CONSTRAINT "PKey" PRIMARY KEY ("ProductID"),
-    CONSTRAINT "Foreign Key" FOREIGN KEY ("UserID")
-        REFERENCES public.vex_user ("UserID") MATCH SIMPLE
+    "product_id" numeric(10,0) NOT NULL,
+    "user_id" numeric(10,0)[] NOT NULL,
+    "product_name" character(255) COLLATE pg_catalog."default",
+    "code" character(255) COLLATE pg_catalog."default",
+    "create_time" date,
+    "is_live" numeric(2,0),
+    "is_delete" numeric(2,0),
+    CONSTRAINT "PKey" PRIMARY KEY ("product_id"),
+    CONSTRAINT "Foreign Key" FOREIGN KEY ("user_id")
+        REFERENCES public.vex_user ("user_id") MATCH SIMPLE
         ON UPDATE NO ACTION
         ON DELETE NO ACTION
 )
@@ -43,30 +53,3 @@ WITH (
     OIDS = FALSE
 )
 TABLESPACE pg_default;
-
-ALTER TABLE public.vex_product
-    OWNER to postgres;
-
--- Table: public.vex_user
-
--- DROP TABLE public.vex_user;
-
-CREATE TABLE public.vex_user
-(
-    "UserName" character(255) COLLATE pg_catalog."default" NOT NULL,
-    "Password" character(255) COLLATE pg_catalog."default",
-    "Name" character(255) COLLATE pg_catalog."default",
-    "Email" character(255) COLLATE pg_catalog."default",
-    "Type" character(255) COLLATE pg_catalog."default",
-    "Creat_Time" date,
-    "UserID" numeric(10,0)[] NOT NULL,
-    "Is_Enable" character(255) COLLATE pg_catalog."default",
-    CONSTRAINT vex_user_pkey PRIMARY KEY ("UserID")
-)
-WITH (
-    OIDS = FALSE
-)
-TABLESPACE pg_default;
-
-ALTER TABLE public.vex_user
-    OWNER to postgres;
