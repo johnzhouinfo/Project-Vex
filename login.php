ce7405ce0dfd76e5ecdd6809b89b95a67859bafa
yufan <shenyufan951009@gmail.com>
2020-02-20 16:21:33 -0500

    temp login-page
    Signed-off-by: yufan <shenyufan951009@gmail.com>

--- login.php
+++ login.php
@@ -1,4 +1,4 @@
-<?php include('server.php') ?>
+
 <!DOCTYPE html>
 <html>
 <head>
@@ -11,7 +11,7 @@
 </div>
 
 <form method="post" action="login.php">
-    <?php include('errors.php'); ?>
+
     <div class="input-group">
         <label>Username</label>
         <input type="text" name="username" >
