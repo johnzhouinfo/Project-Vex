42ddde68d06db490d07de6f98abbaa1a09ce07c4
Xiaoyu Wang <sh.wangxiaoyu@gmail.com>
2020-02-26 21:49:07 -0500

    Initialize basic components of undo function

--- editor.php
+++ editor.php
@@ -10,7 +10,7 @@
     <link rel="icon" href="favicon.ico">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.0/css/bootstrap.min.css">
     <link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css">
-
+    <script src="https://rawgit.com/ArthurClemens/Javascript-Undo-Manager/master/lib/undomanager.js"></script>
     <base href="">
     <title>Vex</title>
 
@@ -24,6 +24,10 @@
     <div id="vex-nav">NAV</div>
     <div id="vex-left-top-project-list">Project List
         <div class="drop">Place Here</div>
+        <div class="undo-container">
+            <input id="undo" class="undo-redo disable" type="button" value="undo" >
+            <input id="redo" class="undo-redo disable" type="button" value="redo">
+        </div>
     </div>
     <div id="vex-component">
 
